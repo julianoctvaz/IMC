0de34446b9d0933d8166906b1cec2c08f8ff86d1
Juliano Vaz <julianoctvaz@gmail.com>
2025-03-26 17:05:27 -0300

    Colocar label e remove main storyboard

--- IMC/IMC/ViewController.swift
+++ IMC/IMC/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    lazy var labelTitle: UILabel = {
+        let label = UILabel()
+        label.text = "IMC"
+        label.textAlignment = .center
+        label.backgroundColor = .red
+        label.font = UIFont.systemFont(ofSize: 30, weight: .bold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+       // Do any additional setup after loading the view.
+        
+        self.view.addSubview(labelTitle)
+        
+        
+        NSLayoutConstraint.activate([
+            labelTitle.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            labelTitle.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
     }
-
-
 }
 
+#Preview {
+    ViewController()
+}
