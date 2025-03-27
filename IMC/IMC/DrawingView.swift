2bb937e1e4506785933005f7885b87102d4b51fd
Juliano Vaz <julianoctvaz@gmail.com>
2025-03-26 21:56:14 -0300

    Desenha diagonal

--- IMC/IMC/DrawingView.swift
+++ IMC/IMC/DrawingView.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class DrawingView: UIView {
 
- //poderia usar path/Bezier
+ //poderia usar path (SwiftUI) /Bezier (curvas mais customizadas)
     
     var corAtual: UIColor = .red
 
@@ -19,6 +19,17 @@ class DrawingView: UIView {
         contexto.setFillColor(corAtual.cgColor)
         let meuRect = CGRect(x: 0, y: 0, width: rect.width / 2, height: rect.height / 2)
         contexto.fill(meuRect)
+        desenharLinhaDiagonal(contexto: contexto, in: meuRect)
+    }
+    
+    private func desenharLinhaDiagonal(contexto: CGContext, in rect: CGRect) {
+        contexto.setStrokeColor(UIColor.blue.cgColor)  // Cor da linha
+        contexto.setLineWidth(4.0)                     // Espessura da linha
+
+        contexto.move(to: CGPoint(x: 0, y: 0))                     // Começa no canto superior esquerdo
+        contexto.addLine(to: CGPoint(x: rect.width, y: rect.height)) // Vai até o canto inferior direito
+
+        contexto.strokePath() // Renderiza a linha no contexto
     }
 
     func updateColorAfterDelay() {
