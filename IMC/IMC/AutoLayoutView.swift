9f36a9278e13ab9b453fcbddb2a3877864e9e49c
Juliano Vaz <julianoctvaz@gmail.com>
2025-03-26 21:44:51 -0300

    faz comentarios para melhor entendimento

--- IMC/IMC/AutoLayoutView.swift
+++ IMC/IMC/AutoLayoutView.swift
@@ -10,9 +10,10 @@ import UIKit
 
 @propertyWrapper final class AutoLayoutView<View: UIView> {
     private lazy var view: View = {
-        let view = View(frame: .zero)
-//        let view = View()
-        view.translatesAutoresizingMaskIntoConstraints = false
+        let view = View(frame: .zero) // no geral o View() ou View.init() tb cria com frame em .zero. O que deixamos explicito aqui é. crie essa view sem tamanho definido (porque eu vou usar Auto Layout para definir depois),
+//        let view = View.init()
+        let view = View()
+        view.translatesAutoresizingMaskIntoConstraints = false // e desligue autoresizing masks para evitar conflitos com Auto Layout.
         return view
     }()
     
