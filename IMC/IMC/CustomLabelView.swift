9653e64e3a5a79e12fa1d2aa1c263b2695ddaf25
Juliano Vaz <26723995+julianoctvaz@users.noreply.github.com>
2025-04-01 20:39:18 -0300

    Update CustomLabelView.swift

--- IMC/IMC/CustomLabelView.swift
+++ IMC/IMC/CustomLabelView.swift
@@ -15,6 +15,7 @@ class CustomLabelView: UIView {
     }()
     
     override init(frame: CGRect) {
+        //se nao setar o translate auto resizing mask aqui para falso temq ue setar onde incorporar!!
         // É o inicializador que usamos de forma padrão da classe UIView quando você cria uma view programaticamente, ou seja, via código, sem usar Storyboard ou XIB.
         super.init(frame: frame)
         setupView()
