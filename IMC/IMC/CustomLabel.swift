aa1a46a8530f1f69c5c00edd0d2eb7d04358df44
Juliano Vaz <26723995+julianoctvaz@users.noreply.github.com>
2025-04-01 22:16:32 -0300

    Update CustomLabel.swift

--- IMC/IMC/CustomLabel.swift
+++ IMC/IMC/CustomLabel.swift
@@ -30,6 +30,8 @@ class CustomLabel: UIView {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    // caso fossemos serialziar algum storyboard
+
     }
     
     
